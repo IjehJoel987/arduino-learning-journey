e638b49a65d15d35c0d112d395e19836cf6db1d1
Ijeh Joel <cijeh.2404322@stu.cu.edu.ng>
2025-10-04 02:38:04 +0100

    Update blink_led.ino

--- Projects/01_blink_led/blink_led.ino
+++ Projects/01_blink_led/blink_led.ino
@@ -1,1 +1,14 @@
+int redpin=13;
+int dt=500;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(redpin,OUTPUT);
+}
 
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(redpin,HIGH);
+delay(dt);
+digitalWrite(redpin,LOW);
+delay(dt);
+}
